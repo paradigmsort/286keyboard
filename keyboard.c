6c7823e608c3250fefc22151c1bffac5df300f3b
W. Owen Parry <paradigmsort@gmail.com>
2013-04-06 14:25:19 -0400

    Matrix keyboard controller
    Senses keypresses by driving rows low and checking if columns follow.

---
+++ keyboard.c
@@ -0,0 +1,71 @@
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+#include "usb_keyboard_debug.h"
+#include "print.h"
+
+#define CPU_PRESCALE(n)	(CLKPR = 0x80, CLKPR = (n))
+
+// input columns are on pins D
+void init_columns(void)
+{
+	DDRD = 0x00; // configure as input (0)
+	PORTD = 0xFF; // configure as pull-up (1)
+}
+
+uint8_t read_columns(void)
+{
+	return ~PIND; // active-low
+}
+
+// for now, only one row
+void select_row(void)
+{
+	DDRF |= (1<<0); 	// output
+	PORTF &= ~(1<<0); 	// low
+	_delay_us(30);
+}
+
+void unselect_rows(void)
+{
+	// switch to high-impedence ie floating input
+	DDRF &= ~(1<<0); 	//input
+	PORTF &= ~(1<<0); 	// floating
+	_delay_us(30);
+}
+
+int main(void)
+{
+	// set for 16 MHz clock
+	CPU_PRESCALE(0);
+
+	// set columns for input and rows as off
+	init_columns();
+	unselect_rows();
+
+	// Initialize the USB, and then wait for the host to set configuration.
+	// If the Teensy is powered without a PC connected to the USB port,
+	// this will wait forever.
+	usb_init();
+	while (!usb_configured()) /* wait */ ;
+
+	// Wait an extra second for the PC's operating system to load drivers
+	// and do whatever it does to actually be ready for input
+	_delay_ms(1000);
+
+	uint8_t prev_cols = 0;
+	uint8_t cols = 0;
+	while (1) {
+		select_row();
+		cols = read_columns();
+		unselect_rows();
+
+		if ((cols & 1) && !(prev_cols&1)) {
+			print("F9 down\n");
+		}
+		if (!(cols&1) && (prev_cols&1)) {
+			print("F9 up\n");
+		}
+		prev_cols = cols;
+	}
+}
