a8ddb0377ba4f7843deffe5845532ae37a26a16c
W. Owen Parry <paradigmsort@gmail.com>
2013-04-14 23:14:51 -0400

    Row 1 names correct.

--- keyboard.c
+++ keyboard.c
@@ -18,8 +18,8 @@
 #define MAX_NAME_LENGTH 16
 
 #define KEYS(oP) \
-	{oP(F8),		oP(F7),	oP(F6),	oP(HOME),	oP(SCROLL_LOCK),	oP(NUM_LOCK),		oP(F10),		oP(F9)}, \
-	{oP(F1),		oP(F2),		oP(F3),			oP(F4), 			oP(F5), 	oP(NONE),	oP(SPACE),	oP(CAPS_LOCK)},\
+	{oP(F8),		oP(F7),		oP(F6),		oP(HOME),	oP(SCROLL_LOCK),	oP(NUM_LOCK),	oP(F10),oP(F9)}, \
+	{oP(CAPS_LOCK),	oP(SPACE),	oP(NONE),	oP(F5), 	oP(F4), 			oP(F3),			oP(F2),	oP(F1)},\
 	{oP(0), 		oP(MINUS),	oP(EQUAL),		oP(BACKSPACE),		oP(TAB),	oP(7),		oP(8),		oP(9)},\
 	{oP(2), 		oP(3),		oP(4),			oP(5),				oP(6),		oP(NONE),	oP(ESC),	oP(0)},\
 	{oP(R), 		oP(F10),	oP(NUM_LOCK),	oP(SCROLL_LOCK),	oP(HOME),	oP(F6),		oP(F7),		oP(F8)},\
